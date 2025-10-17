02f766bf08995ca78d700965f754c3687514dccb
Gabriel Salinas Szada <75760163+gass-git@users.noreply.github.com>
2025-10-17 12:05:20 +0200

    chore: adds the configuration for rollup to be able to bundle

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import PeerDepsExternal from "rollup-plugin-peer-deps-external";
+import typescript from "@rollup/plugin-typescript";
+import { dts } from "rollup-plugin-dts"
+
+const PJ = require("./package.json")
+
+export default [
+  {
+    input: "src/hook.ts",
+    output: [{ file: PJ.module, format: "esm" }],
+    plugins: [
+      PeerDepsExternal(),
+      typescript({ tsconfig: "./tsconfig.json", declaration: false })
+    ],
+    external: ["react", "react-dom"],
+  },
+  {
+    input: "dist/index.d.ts",
+    output: [{file: "dist/index.d.ts", format: "esm"}],
+    plugins: [dts()]
+  }
+]
